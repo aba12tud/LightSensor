532b8d361f026487dc6c373dc1340c541cb64c6f
aba12tud <aithal.abhi@gmail.com>
2013-11-29 21:13:20 +0100

    Dummy commit

--- src/com/lightsensorapp/AlarmReceiver.java
+++ src/com/lightsensorapp/AlarmReceiver.java
@@ -3,7 +3,7 @@ package com.lightsensorapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-//import android.sax.StartElementListener;
+import android.sax.StartElementListener;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver{
